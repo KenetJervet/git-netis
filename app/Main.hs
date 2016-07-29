eae76a07d6af326feea7b40ef3b2e3213db3d234
Savor d'Isavano <anohigisavay@gmail.com>
2016-07-29 17:42:42 +0800

    Basic parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,31 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
-import Lib
+import           Data.Text as T
+import           Options.Applicative
+
+data Command = Project { projectName :: Text
+                       , projectCommand :: ProjectCommand
+                       } deriving Show
+
+data ProjectCommand = MyIssues
+                    deriving Show
+
+argParser :: Parser Command
+argParser = Project <$>
+  (T.pack <$> argument str (metavar "CODE" <> help "Project code"))
+  <*>
+  (const MyIssues <$> argument str (metavar "COMMAND" <> help "Command"))
+
+exec :: Command -> IO ()
+exec (Project {..}) = case projectCommand of
+  MyIssues -> print "MyIssues"
 
 main :: IO ()
-main = someFunc
+main = execParser parser >>= exec
+  where
+    parser = info (helper <*> argParser)
+      ( fullDesc
+        <> progDesc "Print Manage issues, branches and everything"
+      )
---
+++ src/GitNetis/JIRA.hs
@@ -0,0 +1,1 @@
+module GitNetis.JIRA where
